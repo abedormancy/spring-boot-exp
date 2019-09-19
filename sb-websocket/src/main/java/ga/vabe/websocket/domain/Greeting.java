690066f334ecf0271f6c797cc0b5d24b92a97320
abedormancy <abedormancy@gmail.com>
2019-09-19 16:03:49 +0800

    add websocket

---
+++ sb-websocket/src/main/java/ga/vabe/websocket/domain/Greeting.java
@@ -0,0 +1,12 @@
+package ga.vabe.websocket.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Greeting {
+
+    private String content;
+
+}
---
+++ sb-websocket/src/main/java/ga/vabe/websocket/domain/HelloMessage.java
@@ -0,0 +1,10 @@
+package ga.vabe.websocket.domain;
+
+import lombok.Data;
+
+@Data
+public class HelloMessage {
+
+    private String name;
+
+}
